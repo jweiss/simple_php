88a90fe9285230d9b47fb2cf86d62d3dc1b3e270
Thomas Lobinger <tlobinger@users.noreply.github.com>
2014-11-12 11:21:21 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-<h1>Hello re:Invent 2014 from <?php echo gethostname() ?></h1>
+<h1>Hello re:Invent 2014, this is <?php echo gethostname() ?></h1>
 <p>Example PHP application running on <?php echo gethostname() ?>.</p>
 </body>
 </html>
