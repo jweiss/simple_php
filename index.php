d703c4fc234c3eb9b4aee64dd4f8ef81aed78d0e
Jonathan Weiss <jw@innerewut.de>
2014-11-13 14:41:26 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-<h1>Hello re:Invent 2014, this is <?php echo gethostname() ?> !new code here!</h1>
+<h1>Hello re:Invent 2014, this is <?php echo gethostname() ?> - hello Jenkins</h1>
 <p>Example PHP application running on <?php echo gethostname() ?>.</p>
 </body>
 </html>
